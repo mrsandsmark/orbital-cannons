2b2edcb863f02763f5f299e7b465be893d239092
ANNOProfi <ANNOProfi@t-online.de>
2025-08-11 23:18:58 +0200

    Added namespace

--- Source/OCDefOfs.cs
+++ Source/OCDefOfs.cs
@@ -1,27 +1,30 @@
 ﻿using RimWorld;
 using Verse;
 
-[DefOf]
-public static class OCDefOfs
+namespace Orbital_Cannons
 {
-    //Gauss Cannon
-    public static ThingDef Turret_GaussCannonReal;
-    public static ThingDef Gun_GaussCannonReal;
-    //Autocannon
-    public static ThingDef Turret_GaussCannon;
-    public static ThingDef Gun_GravShipTurret;
-    //CIWS
-    public static ThingDef Turret_Gauss;
-    public static ThingDef Gun_GravShipCannon;
-    //Artillery
-    public static ThingDef GaussCannon_Mortar;
-    public static ThingDef GravShip_Mortar;
-    //Big Beam
-    public static ThingDef AntiShipBeam_Turret;
-    public static ThingDef Gun_AntiShipBeam;
-    //Small Beam
-    public static ThingDef OrbitalBeam_Turret;
-    public static ThingDef Gun_OrbitalBeam;
-    //Gravship Targeter
-    public static ThingDef GravShipTargeter;
+    [DefOf]
+    public static class OCDefOfs
+    {
+        //Gauss Cannon
+        public static ThingDef Turret_GaussCannonReal;
+        public static ThingDef Gun_GaussCannonReal;
+        //Autocannon
+        public static ThingDef Turret_GaussCannon;
+        public static ThingDef Gun_GravShipTurret;
+        //CIWS
+        public static ThingDef Turret_Gauss;
+        public static ThingDef Gun_GravShipCannon;
+        //Artillery
+        public static ThingDef GaussCannon_Mortar;
+        public static ThingDef GravShip_Mortar;
+        //Big Beam
+        public static ThingDef AntiShipBeam_Turret;
+        public static ThingDef Gun_AntiShipBeam;
+        //Small Beam
+        public static ThingDef OrbitalBeam_Turret;
+        public static ThingDef Gun_OrbitalBeam;
+        //Gravship Targeter
+        public static ThingDef GravShipTargeter;
+    }
 }
